75384a6c69a8d54ceb73aba67e7755b6fdcc6c7b
1vivy <1vivy@tutanota.com>
2026-07-09 02:31:39 +0000

    camera: cryptoeng App-Lock HAL payload + vendor sepolicy
    Change-Id: Ie9657141375dd43dd1ae3db4a2f6040095230cd2

--- camera-sm8850-common-vendor.mk
+++ camera-sm8850-common-vendor.mk
@@ -6,7 +6,19 @@ PRODUCT_SOONG_NAMESPACES += \
     proprietary/vendor/oneplus/camera-sm8850-common
 
 PRODUCT_COPY_FILES += \
-    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/etc/permissions/vendor-oplus-hardware-cryptoeng.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/vendor-oplus-hardware-cryptoeng.xml \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b00:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b00 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b01:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b01 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b02:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b02 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b03:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b03 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b04:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b04 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b05:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b05 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b06:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b06 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b07:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b07 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b08:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b08 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.mdt:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.mdt \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/permissions/vendor-oplus-hardware-cryptoeng.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/vendor-oplus-hardware-cryptoeng.xml
 
 PRODUCT_PACKAGES += \
     libpenguin \
@@ -60,8 +72,18 @@ PRODUCT_PACKAGES += \
     libst_sticker_jni \
     libsuperNight.oplus \
     libvr_amb_engine \
+    vendor.oplus.hardware.cryptoeng-V1-ndk \
+    vendor.oplus.hardware.cryptoeng@1.0 \
     libOplusSecurity \
+    lib_cryptoeng_api \
+    libqsee_keybox_ca \
+    vendor.oplus.hardware.cryptoeng-V1-ndk_odm \
+    vendor.oplus.hardware.cryptoeng@1.0_odm \
     OplusCamera \
     OppoGallery2 \
     com.oplus.camera.unit.sdk.adapter \
-    com.oplus.camera.unit.sdk
+    com.oplus.camera.unit.sdk \
+    vendor.oplus.hardware.cryptoeng-V1-java \
+    vendor.oplus.hardware.cryptoeng-V1.0-java \
+    manifest_oplus_cryptoeng.xml \
+    vendor.oplus.hardware.cryptoeng@1.0-service
