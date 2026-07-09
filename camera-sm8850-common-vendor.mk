a827a1d3449c89575df6b742129cca9e254c1c5d
1vivy <1vivy@tutanota.com>
2026-07-09 02:35:49 +0000

    camera: ship FIDO CA HAL chain + vendor sepolicy
    Change-Id: Iafbefc9bf02dc0c65a07ed9946109b25932f3747

--- camera-sm8850-common-vendor.mk
+++ camera-sm8850-common-vendor.mk
@@ -6,6 +6,8 @@ PRODUCT_SOONG_NAMESPACES += \
     proprietary/vendor/oneplus/camera-sm8850-common
 
 PRODUCT_COPY_FILES += \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/etc/init/vendor.oplus.hardware.fido.fido2ca@1.0-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.fido.fido2ca@1.0-service.rc \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/etc/init/vendor.oplus.hardware.fido.fidoca@1.0-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.fido.fidoca@1.0-service.rc \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/etc/permissions/vendor-oplus-hardware-cryptoeng.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/vendor-oplus-hardware-cryptoeng.xml \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b00:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b00 \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b01:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b01 \
@@ -17,6 +19,26 @@ PRODUCT_COPY_FILES += \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b07:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b07 \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.b08:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.b08 \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/cryptoeng.mdt:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/cryptoeng.mdt \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b00:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b00 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b01:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b01 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b02:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b02 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b03:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b03 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b04:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b04 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b05:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b05 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b06:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b06 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b07:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b07 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.b08:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.b08 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidoctap.mdt:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidoctap.mdt \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b00:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b00 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b01:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b01 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b02:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b02 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b03:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b03 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b04:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b04 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b05:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b05 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b06:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b06 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b07:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b07 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.b08:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.b08 \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/odm/firmware/secure_ta/fidotap.mdt:$(TARGET_COPY_OUT_ODM)/firmware/secure_ta/fidotap.mdt \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
     proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/permissions/vendor-oplus-hardware-cryptoeng.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/vendor-oplus-hardware-cryptoeng.xml
 
@@ -76,9 +98,13 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.cryptoeng@1.0 \
     libOplusSecurity \
     lib_cryptoeng_api \
+    libfido2_factory \
+    libfido_factory \
     libqsee_keybox_ca \
     vendor.oplus.hardware.cryptoeng-V1-ndk_odm \
     vendor.oplus.hardware.cryptoeng@1.0_odm \
+    vendor.oplus.hardware.fido.fido2ca-V1-ndk \
+    vendor.oplus.hardware.fido.fidoca-V1-ndk \
     OplusCamera \
     OppoGallery2 \
     com.oplus.camera.unit.sdk.adapter \
@@ -86,4 +112,8 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.cryptoeng-V1-java \
     vendor.oplus.hardware.cryptoeng-V1.0-java \
     manifest_oplus_cryptoeng.xml \
-    vendor.oplus.hardware.cryptoeng@1.0-service
+    manifest_oplus_fido.xml \
+    manifest_oplus_fido2.xml \
+    vendor.oplus.hardware.cryptoeng@1.0-service \
+    vendor.oplus.hardware.fido.fido2ca@1.0-service \
+    vendor.oplus.hardware.fido.fidoca@1.0-service
