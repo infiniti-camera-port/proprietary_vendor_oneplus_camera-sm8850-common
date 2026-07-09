1c169b43fb45279f9ffec30288be0d7d3bca2fce
1vivy <1vivy@tutanota.com>
2026-07-09 02:14:07 +0000

    camera: fix OplusCamera.apk crash on open + themed icon
    Change-Id: I6456a7054f5b0d4e9b3eaa66fa97b60c3a54af99

---
+++ camera-sm8850-common-vendor.mk
@@ -0,0 +1,66 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    proprietary/vendor/oneplus/camera-sm8850-common
+
+PRODUCT_COPY_FILES += \
+    proprietary/vendor/oneplus/camera-sm8850-common/proprietary/system_ext/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml
+
+PRODUCT_PACKAGES += \
+    libpenguin \
+    libAPSClient-alog-jni \
+    libAPSClient-cmd-jni-extension.oplus \
+    libAPSClient-cmd-jni \
+    libAPSClient-jni \
+    libAncFilter_jni \
+    libAncHumBokeh-jni \
+    libAncHumanDoubleExposure-jni \
+    libAncHumanRetain-jni_v2 \
+    libAncHumanSegFigureFusion-jni \
+    libAncHumanVideo-jni \
+    libApsFaceBeautyPreviewProductJni \
+    libAvatarEngineRender \
+    libAvatarEngineRenderNative \
+    libCombineLut \
+    libCombineLutJni \
+    libFileExtender-jni \
+    libHdrTransform-platform-jni \
+    libHeifEncoderWrapper \
+    libNativeWinBuffExchange \
+    libOplusBlurPreviewJNI \
+    libOplusStringJNI-extension.oplus \
+    libOplusStringJNI \
+    libSuperTextWrapper_system_ext \
+    libXDocProcessSDK-jni \
+    libXDocProcessSDK_system_ext \
+    libYTCommon_system_ext \
+    libapssuspend-jni.oplus \
+    libarcsoft_panorama_burstcapture \
+    libarcsoft_wideselfie \
+    libbinauralrenderer_wrapper.qti \
+    libextendfile_system_ext \
+    libhoaeffects.qti \
+    libhoaeffects_csim \
+    libjni_burstpmk \
+    libjni_wideselfie \
+    libjnisingleblur_api \
+    libjnistblur_api \
+    libmpbase_system_ext \
+    liboplusheifwriter \
+    libortc_so.oplus \
+    libsatellite_audio_jni.oplus \
+    libsingle_camera_bokeh2_native \
+    libsingle_camera_bokeh_native \
+    libsoundtouch.oplus \
+    libsoundtouch_jni.oplus \
+    libst_mobile \
+    libst_sticker_jni \
+    libsuperNight.oplus \
+    libvr_amb_engine \
+    libOplusSecurity \
+    OplusCamera \
+    OppoGallery2 \
+    com.oplus.camera.unit.sdk.adapter \
+    com.oplus.camera.unit.sdk
